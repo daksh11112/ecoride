435d08e3b74fb539ec6901c5793cbd87b1141ca8
Chaitanya <aggarwalchaitanya1679@gmail.com>
2024-05-28 12:09:59 +0530

    deployment 2

--- index.js
+++ index.js
@@ -257,11 +257,11 @@ app.post('/searchRides', authenticateUser, async (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 
 app.listen(host, () => {
